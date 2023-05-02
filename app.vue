47f2922050de2292aabdcf88b90ef312bfd39e16
aaronzdavis <aaronzdavis@gmail.com>
2023-05-02 11:03:08 -0700

    add square payment sdk

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     Hello World!
+
+    <form id="card-form" class="payment-form" onsubmit="alert('hello!')">
+      <div id="card-container"></div>
+      <button type="submit">Pay</button>
+    </form>
   </div>
 </template>
 
@@ -9,6 +14,25 @@ useHead({
   link: [{
     rel: 'manifest',
     href: 'manifest.json'
+  }],
+  script: [{
+    hid: 'square-sdk',
+    src: 'https://sandbox.web.squarecdn.com/v1/square.js',
   }]
 })
+
+onMounted(async () => {
+  const SQUARE_APPLICATION_ID = "sandbox-sq0idb-gQCtis_1gvFl5rguGUXNRQ";
+  const SQUARE_ECOMM_LOCATION_ID = "L5R1RKYHWDVV0";
+
+  const payments = Square.payments(
+    SQUARE_APPLICATION_ID,
+    SQUARE_ECOMM_LOCATION_ID
+  )
+
+  const card = await payments.card()
+  await card.attach('#card-container')
+})
+
+
 </script>
